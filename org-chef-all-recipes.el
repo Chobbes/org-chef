9923304cfe52c9987b676059cf5aa47ca45567ed
Calvin Beck <hobbes@ualberta.ca>
2018-02-26 20:49:33 -0700

    Converted all-recipes to use dom.el

--- org-chef-all-recipes.el
+++ org-chef-all-recipes.el
@@ -35,39 +35,39 @@
 ;;; Code:
 
 
-(defun org-chef-all-recipes-extract-name (ast)
-  "Get the name of a recipe from an allrecipes elquery AST."
-  (elquery-text (car (elquery-$ "[itemprop=name]" ast))))
+(defun org-chef-all-recipes-extract-name (dom)
+  "Get the name of a recipe from an allrecipes DOM."
+  (dom-text (car (dom-by-class dom "^recipe-summary__h1$"))))
 
 
-(defun org-chef-all-recipes-extract-ingredients (ast)
-  "Get the ingredients for a recipe from an allrecipes elquery AST."
-  (reverse (mapcar 'elquery-text (elquery-$ "[itemprop=ingredients]" ast))))
+(defun org-chef-all-recipes-extract-ingredients (dom)
+  "Get the ingredients for a recipe from an allrecipes DOM."
+  (mapcar 'dom-text (dom-elements dom 'itemprop "^ingredients$")))
 
 
-(defun org-chef-all-recipes-extract-servings (ast)
-  "Get the number of servings for a recipe from an allrecipes elquery AST."
-  (car (mapcar (lambda (node) (elquery-prop node "content")) (elquery-$ "[itemprop=recipeYield]" ast))))
+(defun org-chef-all-recipes-extract-servings (dom)
+  "Get the number of servings for a recipe from an allrecipes DOM."
+  (dom-attr (car (dom-by-id dom "metaRecipeServings")) 'content))
 
 
-(defun org-chef-all-recipes-extract-prep-time (ast)
-  "Get the amount of prep-time for a recipe from an allrecipes elquery AST."
-  (mapcar (lambda (node) (mapcar 'elquery-text (elquery-children node))) (elquery-$ "[itemprop=prepTime]" ast)))
+(defun org-chef-all-recipes-extract-prep-time (dom)
+  "Get the amount of prep-time for a recipe from an allrecipes DOM."
+  (dom-texts (car (dom-elements dom 'itemprop "^prepTime$"))))
 
 
-(defun org-chef-all-recipes-extract-cook-time (ast)
-  "Get the amount of cook-time for a recipe from an allrecipes elquery AST."
-  (mapcar (lambda (node) (mapcar 'elquery-text (elquery-children node))) (elquery-$ "[itemprop=cookTime]" ast)))
+(defun org-chef-all-recipes-extract-cook-time (dom)
+  "Get the amount of cook-time for a recipe from an allrecipes DOM."
+  (dom-texts (car (dom-elements dom 'itemprop "^cookTime$"))))
 
 
-(defun org-chef-all-recipes-extract-ready-in (ast)
-  "Get the total amount of time for a recipe from an allrecipes elquery AST."
-  (mapcar (lambda (node) (mapcar 'elquery-text (elquery-children node))) (elquery-$ "[itemprop=totalTime]" ast)))
+(defun org-chef-all-recipes-extract-ready-in (dom)
+  "Get the total amount of time for a recipe from an allrecipes DOM."
+  (dom-texts (car (dom-elements dom 'itemprop "^totalTime$"))))
 
 
-(defun org-chef-all-recipes-extract-directions (ast)
-  "Get the directions for a recipe from an allrecipes elquery AST."
-  (reverse (org-chef-remove-empty-strings (mapcar 'elquery-text (elquery-$ "[class=recipe-directions__list--item]" ast)))))
+(defun org-chef-all-recipes-extract-directions (dom)
+  "Get the directions for a recipe from an allrecipes DOM."
+  (org-chef-remove-empty-strings (mapcar 'dom-text (dom-by-class dom  "^recipe-directions__list--item$"))))
 
 
 (defun org-chef-all-recipes-fetch (url)
@@ -84,14 +84,15 @@ This returns an alist with the following keys:
 - directions
 - source-url"
   (with-current-buffer (url-retrieve-synchronously url)
-    (let  ((ast (elquery-read-string (buffer-string))))
-      `((name . ,(org-chef-all-recipes-extract-name ast))
-        (ingredients . ,(org-chef-all-recipes-extract-ingredients ast))
-        (servings . ,(org-chef-all-recipes-extract-servings ast))
-        (prep-time . ,(org-chef-all-recipes-extract-prep-time ast))
-        (cook-time . ,(org-chef-all-recipes-extract-cook-time ast))
-        (ready-in . ,(org-chef-all-recipes-extract-ready-in ast))
-        (directions . ,(org-chef-all-recipes-extract-directions ast))
+    (let  ((dom (libxml-parse-html-region (point-min) (point-max))))
+
+      `((name . ,(org-chef-all-recipes-extract-name dom))
+        (ingredients . ,(org-chef-all-recipes-extract-ingredients dom))
+        (servings . ,(org-chef-all-recipes-extract-servings dom))
+        (prep-time . ,(org-chef-all-recipes-extract-prep-time dom))
+        (cook-time . ,(org-chef-all-recipes-extract-cook-time dom))
+        (ready-in . ,(org-chef-all-recipes-extract-ready-in dom))
+        (directions . ,(org-chef-all-recipes-extract-directions dom))
         (source-url . ,url)))))
 
 
